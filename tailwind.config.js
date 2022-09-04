5f5beaa807514ccaf7e1e635a3183385e16fa5cc
Humberto Martins <humberto@consilio.com.br>
2022-09-04 13:51:26 +0200

    chore(update): change folders app to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
